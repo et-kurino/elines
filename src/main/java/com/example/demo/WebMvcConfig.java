067683414e6093d476af97eed8f069e7b5b27b31
dai <>
2018-03-13 11:26:24 +0900

    test

--- src/main/java/com/example/demo/WebMvcConfig.java
+++ src/main/java/com/example/demo/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.GzipResourceResolver;
