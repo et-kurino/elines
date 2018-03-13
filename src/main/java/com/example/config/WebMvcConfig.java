3ce0acded3ed743e952d48ffe81218029019c5ea
et.kurino <et.kurino@gmail.com>
2018-03-13 10:28:19 +0900

    config error

---
+++ src/main/java/com/example/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // webjars の定義 js bootstrap
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/")
+                    .resourceChain(false) // 自動で WebJarsResourceResolver が有効化される。
+                    .addResolver(new GzipResourceResolver()); // gz ファイルへのアクセス有効化。
+        
+   }
+    
+   @Override
+   public void addViewControllers(ViewControllerRegistry registry) {
+	   registry.addViewController("/hello").setViewName("hello");
+	   registry.addViewController("/").setViewName("hello");
+
+   }
+}
